43bd544217cf12238668081da22646b017414a22
Mohamed Hashim <mmhashim139@gmail.com>
2020-11-01 01:49:11 +0000

    Connect flask with mongoDB , render the data in html file

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 # import application packages
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, redirect,
+    request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+# app configration
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def init():
-    return "Hello Hikers ... are you ready !!"
+# show place form DB to test MongoDB connection
+@app.route("/all_places")
+def all_places():
+    places = mongo.db.places.find()
+    return render_template("places.html", places=places)
 
 
 # make sure to debug= False before submit
