1126c50497b4701c3f7dd5b64d73201094ca9a88
Mohamed Hashim <mmhashim139@gmail.com>
2020-11-01 00:46:48 +0000

    Started with Creation of Flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+# import application packages
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def init():
+    return "Hello Hikers ... are you ready !!"
+
+
+# make sure to debug= False before submit
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
