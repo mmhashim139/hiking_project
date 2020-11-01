46f8b49d23a292f9d9bde763652cd2f1c2e3019a
Mohamed Hashim <mmhashim139@gmail.com>
2020-11-01 06:40:52 +0000

    customize slider with jquery

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,13 @@
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
-        
+    // Slider
+  $(document).ready(function(){
+    $('.slider').slider({
+        indicators: false,
+        height: 550,
+        transition: 500,
+        interval: 5000
+        });
+  });
+       
