56f7ad65b0a39975e6c20cbd3202664cf2748dd7
Mohamed Hashim <mmhashim139@gmail.com>
2020-11-17 19:58:46 +0000

    App debug to False

--- app.py
+++ app.py
@@ -394,4 +394,4 @@ def get_avg_rating(id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
